5a53565d3cbf2cc2ab3ea7eb82754da78f26e28b
Fahim Chowdhury <88318197+fahim6a@users.noreply.github.com>
2022-09-21 03:01:52 +0600

    Add files via upload

---
+++ Practice_on_methods.java
@@ -0,0 +1,70 @@
+public class Practice_on_methods {
+//    //program for multiplication table
+//    static int mul_table(int n){
+//        for (int i=0; i<=5;i++){
+//            System.out.printf("the %d X %d is: %d\n",n,i,n*i);
+//            System.out.println(" ");
+//        }
+//        return 1;
+//    }
+
+//    //program for print a pattern
+//    static int pattern(int n){
+//        for (int i=0; i<n; i++){
+//            for (int j=0; j<i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        return 1;
+//    }
+
+//// write a program to print sum of first n natural numbers
+//    //natural numbers formula is n*(n+1)/2
+//    static int natural_number(int n){
+//        int sum=0;
+//        for (int i=1; i<=n; i++){
+//            sum = n*(n+1)/2;
+//        }
+//        System.out.println("the sum is: "+sum);
+//        return n;
+//    }
+
+//    //write a program to print reverse pattern
+//
+//    static  int rev_pattern(int n){
+//      for (int i=4; i>0; i--){
+//          for (int j=0; j<i; j++){
+//              System.out.print("\t*");
+//          }
+//          System.out.println();
+//      }
+//      return 1;
+//    }
+
+    //write a program to print nth numbers of fibonacci series using recursion
+    //fibonacci series 0,1,1,2,3,5,8,13,21,34
+//    static int fib(int n){
+//      if(n==1){
+//          return 0;
+//      } else if (n==2) {
+//          return 1;
+//      }
+//      else {
+//          return fib(n-1)+fib(n-2);
+//      }
+//    }
+    public static void main(String[] args) {
+         // mul_table(7);
+//        int n=5;
+//        pattern(n);
+//        int n=16;
+//        natural_number(n);
+//        int n=4;
+//        rev_pattern(n);
+
+//       int result = fib(5);
+//        System.out.println(result);
+
+    }
+}
