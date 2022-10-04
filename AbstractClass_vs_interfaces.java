4ec89697441133409555d84a8480b691379c1e7e
Fahim Chowdhury <88318197+fahim6a@users.noreply.github.com>
2022-10-05 03:58:05 +0600

    Add files via upload

---
+++ AbstractClass_vs_interfaces.java
@@ -0,0 +1,13 @@
+/*a class has no implementation is called abstract
+* you can not make parent class object
+* interface
+* syntax
+ * class className implements interfaceName
+ * here implements is a keyword
+* */
+/*interface*/
+public class AbstractClass_vs_interfaces {
+    public static void main(String[] args) {
+
+    }
+}
